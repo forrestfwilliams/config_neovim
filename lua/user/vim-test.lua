7a47733c2a0194385b1ef6bf75b2cb47a20f3784
Forrest Williams <ffwilliams2@alaska.edu>
2022-04-25 10:22:24 -0500

    configure vim-test for conda environments

--- lua/user/vim-test.lua
+++ lua/user/vim-test.lua
@@ -1,5 +1,5 @@
 -- apparently vim addons don't need to be required
 
 vim.g["test#python#runner"] = "pytest"
-vim.g["test#python#pytest#executable"] = "docker exec TESTING /opt/conda/bin/pytest"
+vim.g["test#python#pytest#executable"] = vim.env.CONDA_PREFIX .. "/bin/pytest" 
 
